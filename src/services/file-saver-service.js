8378a8983b5deefe44c3f0d7fbd8ae91b058090d
Sam Huo <hhuo213@hotmail.com>
2020-04-18 19:59:51 -0700

    Quality Of life file name fix

--- src/services/file-saver-service.js
+++ src/services/file-saver-service.js
@@ -8,6 +8,28 @@ export class FileSaverService {
 
 	static getFileName() {
 		const now = new Date(Date.now());
-		return `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}.txt`;
+		return `${now.getFullYear()}-${FileSaverService.getMonth(now)}-${FileSaverService.getDay(now)}.txt`;
+	}
+
+	static getMonth(date) {
+		const month = date.getMonth() + 1;
+		let monthString = month;
+
+		if (month < 10) {
+			monthString = `0${month}`;
+		}
+
+		return monthString;
+	}
+
+	static getDay(date) {
+		const day = date.getDate();
+		let dayString = day;
+
+		if (day < 10) {
+			dayString = `0${day}`;
+		}
+
+		return dayString;
 	}
 }
